df564e55e7fc84a0e313c21f6941361d2244c129
Apprentice <apprentice@dbc-20.local>
2013-10-09 12:25:48 -0700

    passed first test

--- spec/Models/user_spec.rb
+++ spec/Models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe "a user" do
-  it "exits" do
-  expect()
-
-
+  let(:user1) {FactoryGirl.build(:user)}
+  it "factory exists" do
+  p user1
+  expect(user1).to be_valid
+  end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :user do |f|
+    f.password { Faker::Lorem.characters(6..20) }
+    f.username { Faker::Internet.user_name }
+  end
+end
