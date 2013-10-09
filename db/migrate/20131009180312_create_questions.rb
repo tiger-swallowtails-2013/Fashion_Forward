4d24df0c091756fad7fe2063ae8cfe35049091e6
Apprentice <apprentice@dbc-20.local>
2013-10-09 11:15:38 -0700

    add create_ questions and answers

--- db/migrate/20131009180312_create_questions.rb
+++ db/migrate/20131009180312_create_questions.rb
@@ -1,4 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :title
+      t.string :body
+      t.timestamps
+    end
   end
 end
--- db/migrate/20131009180320_create_answers.rb
+++ db/migrate/20131009180320_create_answers.rb
@@ -1,4 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+      create_table :answers do |t|
+      t.string :body
+      t.boolean :YN
+      t.timestamps
+    end
   end
 end
