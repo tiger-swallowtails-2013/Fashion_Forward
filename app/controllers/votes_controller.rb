f98fdb47de4bcebd5f9346704f43e7249415e791
Apprentice <apprentice@dbc-21.local>
2013-10-11 18:18:47 -0700

    removed unneccessary strong params

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,18 +4,8 @@ class VotesController < ApplicationController
   end
 
   def create
-    answer = Answer.find(answer_params[:answer_id])
-    p params
-    p current_user
-    p answer
+    answer = Answer.find(params[:answer_id])
     @vote = answer.votes.create(user_id: current_user.id)
-    p @vote.errors
     redirect_to question_answers_path(answer.question)
-  end
-
-
-  private
-  def answer_params
-    params.permit(:answer_id)
   end
 end
