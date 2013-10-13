d96cc3e9d3cea087d648520ee898a1fa2e1f0b74
Zee Spencer <zee@zeespencer.com>
2013-10-12 19:44:02 -0700

    Add a test for posting a question with invalid params

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,13 +1,27 @@
 require "spec_helper"
 
 describe QuestionsController do
-  it "creates a question with valid params via the create path" do
-    expect { post :create, {
-      question: {
-        title: "This is a question",
-        body: "It has a body that looks like this"
-        }
-      }
-    }.to change{ Question.count }.by(1)
+  describe "#create" do
+    let(:question_attributes) { FactoryGirl.attributes_for(question_factory) }
+    def send_create_question_request
+      post :create, { question: question_attributes }
+    end
+    context "with a valid question" do
+      let(:question_factory) { :valid_question }
+      it "saves the question" do
+        expect {
+          send_create_question_request
+        }.to change{ Question.count }.by(1)
+      end
+    end
+    context "with an invalid question" do
+      let(:question_factory) { :invalid_question }
+      it "does not create the question" do
+        expect {
+          send_create_question_request
+        }.not_to change{ Question.count }
+      end
+    end
   end
+
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,17 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :question do |f|
-    f.title { Faker::Lorem.characters(3..7) }
-    f.body { Faker::Lorem.sentences(sentence_count = 2) }
-    f.user_id { 1 }
+  factory :question do
+    title Faker::Lorem.characters(3..7)
+    body Faker::Lorem.sentences(sentence_count = 2).join("\n")
+    user_id { 1 }
+
+    factory :valid_question do
+    end
+
+    factory :invalid_question do
+      title ""
+      body ""
+    end
   end
 end
