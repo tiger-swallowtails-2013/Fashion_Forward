feb74f7d3e0ab5cbe6cb2c177c98ed238e721512
Apprentice <apprentice@dbc-21.local>
2013-10-13 19:06:08 -0700

    improve design of questions index to include accordian styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-    @question.user_id = 1 #change once sessions has been enabled
+    @question.user_id = current_user.id
 
     if @question.save
       redirect_to question_answers_path(@question.id)
